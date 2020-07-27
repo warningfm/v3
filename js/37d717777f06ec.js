8b32f942058b69728ef2f1ffe69e89dfdb2e07a2
warningfm <warningfm@gmail.com>
2020-07-27 23:28:51 +0700

    Update 37d717777f06ec.js

--- js/37d717777f06ec.js
+++ js/37d717777f06ec.js
@@ -12,7 +12,7 @@
         version: 2,
         sid: 1,
         statsPath: 'stats',
-        //directStreamURL: 'https://warningfm.herokuapp.com/?q=http://cast2.bintangtenggarafm.com:1057/stream',
+        directStreamURL: 'https://zet.pluginsandthemes.ro/http://cast2.bintangtenggarafm.com:1057/stream',
         autoUpdate: true,
         autoPlay: true,
         betaProxies: true,
