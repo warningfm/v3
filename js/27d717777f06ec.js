6cd360a241b55b9ebf145620d17d516fbb4e5aaa
warningfm <warningfm@gmail.com>
2020-08-08 03:16:04 +0700

    Update 27d717777f06ec.js

--- js/27d717777f06ec.js
+++ js/27d717777f06ec.js
@@ -12,7 +12,7 @@
         version: 2,
         sid: 1,
         statsPath: 'stats',
-        directStreamURL: 'https://api.codetabs.com/v1/proxy?quest=http://live.radiobintangtenggara.com:8000/mbahnunungonline',
+        directStreamURL: 'https://castprox.herokuapp.com/http://live.radiobintangtenggara.com:8000/mbahnunungonline',
         autoUpdate: true,
         autoPlay: true,
         betaProxies: true,
