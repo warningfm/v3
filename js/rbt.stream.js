1c4986540ea073e2c3699c386953eb1cc37a9669
warningfm <warningfm@gmail.com>
2020-10-14 22:29:54 +0700

    Update rbt.stream.js

--- js/rbt.stream.js
+++ js/rbt.stream.js
@@ -11,6 +11,7 @@
         protocol: 'http',
         directStreamURL: 'https://ssg.streamingmurah.com:9560/rbtfm',
         defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
+        statusBar: false,
         colors: 'dynamic',
         theme: 'dark',
         autoPlay: true,
