a5847fd10ab9494b87b6fac31112ed18ca78b96f
warningfm <warningfm@gmail.com>
2020-10-16 01:57:30 +0700

    Update 27d717772f06ec.js

--- js/27d717772f06ec.js
+++ js/27d717772f06ec.js
@@ -1,5 +1,3 @@
-// =============================================== //
-// - Remove any active classes (yellow boxes)
 // - please add your options (host, port, version, etc..)
 // - Destroy any pervious Kast
 // - On example box click, call Kast
@@ -10,33 +8,19 @@
         port: 9720,
         protocol: 'http',
         version: 1,
-        sid: 1,
-        statsPath: 'stats',
         directStreamURL: 'https://proxy.webdesign-flash.ro/?q=http://live.sg.mcfmbwi.com:9720/mcfm',
-        autoUpdate: true,
-        autoPlay: true,
-        betaProxies: true,
-        mobileCare: false,
-        minimizeMaximize: false,
-        muteUnmute: false,
+        defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
         statusBar: false,
         startTemplate: 'maximized',
-        irrelevantWords: ['false'],
-        defaultArtwork: ['https://2.bp.blogspot.com/-lRkFcc6_lYI/W5Vh1v8GwTI/AAAAAAAAJaY/u2FGIl8Gk4Udqqyg7fZ_PTa8p329strVgCLcBGAs/s1600/44-721522597654ctljd.png'],
-        offlineCheck: false,
         language: {
             offlineText: 'Temporarily Offline',
             playedText: '<font face="Georgia" color="blue">Wes Mari Diputer :</font>',
             unknownTrackText: '97.2 - c^o^d^e^l^i^s^t^.^c^c', 
-            unknownArtistText: 'RADIO MC FM' 
-        }, 
-        overHTTPS: true,
+            unknownArtistText: 'Radio MC FM' 
+        },
         position: 'left',
-        container: 'body',
         colors: 'dynamic',
         theme: 'dark',
-        ui: 'colored',
-        played: false,
-        artwork: true,
-        continuous: true
-    })
+        autoPlay: true,
+        played: false
+      })
