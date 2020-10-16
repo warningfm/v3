52f4f6c061d1a4a063472caeb2ec971ab470ef42
warningfm <warningfm@gmail.com>
2020-10-16 13:44:25 +0700

    Update citrafm.stream.js

--- js/citrafm.stream.js
+++ js/citrafm.stream.js
@@ -1,5 +1,3 @@
-// =============================================== //
-// - Remove any active classes (yellow boxes)
 // - please add your options (host, port, version, etc..)
 // - Destroy any pervious Kast
 // - On example box click, call Kast
@@ -9,27 +7,21 @@
         host: '\x6c\x69\x76\x65\x2e\x63\x69\x74\x72\x61\x66\x6d\x72\x61\x64\x69\x6f\x2e\x63\x6f\x6d',
         port: 9882,
         protocol: 'http',
-        version: 2,
-        sid: 1,
-        statsPath: 'stats',
-        autoUpdate: true,
-        betaProxies: true,
-        mobileCare: false,
-        minimizeMaximize: false,
-        muteUnmute: false,
+        version: 1,
+        directStreamURL: 'https://corsradio.herokuapp.com/?q=http://live.citrafmradio.com:9882/stream',
+        defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
         statusBar: false,
         startTemplate: 'maximized',
-        irrelevantWords: false,
-        defaultArtwork: ['https://lh3.googleusercontent.com/MjH8Hd86LGfBfPhwZznGzjblrvhcNvTQ-8wN59KUXCQLAarnxL-_xATQ0VQL-WQSuU5H=w1366-h654'],
-        offlineCheck: false,
-        overHTTPS: true,
+        language: {
+            offlineText: 'Temporarily Offline',
+            playedText: '<font face="Georgia" color="blue">Wes Mari Diputer :</font>',
+            unknownTrackText: 'Bondowoso - c^o^d^e^l^i^s^t^.^c^c', 
+            unknownArtistText: 'Radio Citra' 
+        },
         position: 'left',
-        container: 'body',
         colors: 'dynamic',
-        theme: 'dynamic',
-        ui: 'colored',
-        played: false,
+        theme: 'dark',
         autoPlay: true,
-        continuous: true
-    })
+        played: false
+      })
 
