1810a970d4f575cc6ff55291181062936e37a91a
warningfm <warningfm@gmail.com>
2021-07-06 12:26:26 +0700

    Update 27d717777f06ec.js

--- js/27d717777f06ec.js
+++ js/27d717777f06ec.js
@@ -10,7 +10,7 @@
         port: 9560,
         protocol: 'http',
         version: 1,
-        directStreamURL: 'https://ssg.streamingmurah.com:9560/rbt',
+        directStreamURL: 'https://ssg.streamingmurah.com:9560/rbtfm',
         defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
         statusBar: false,
         startTemplate: 'maximized',
