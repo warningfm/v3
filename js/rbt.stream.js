b5b1f6b2606269b9d47fa493052decacc5f1e9a2
Radio Warning FM <warningfm@gmail.com>
2023-12-25 12:54:43 +0700

    Update rbt.stream.js

--- js/rbt.stream.js
+++ js/rbt.stream.js
@@ -6,11 +6,11 @@
 // =============================================== //
 
     $.kast({
-        host: 'cast1.servcast.net',
-        port: 5398,
-        protocol: 'http',
-        version: 1,
-        directStreamURL: 'https://cast4.mbahnunungonline.net/proxy/mbahnunung55?mp=/stream',
+        host: 'ssg.streamingmurah.com',
+        port: 9560,
+        protocol: 'https',
+        version: 2,
+        //directStreamURL: 'https://cast4.mbahnunungonline.net/proxy/mbahnunung55?mp=/stream',
         defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
         statusBar: false,
         startTemplate: 'maximized',
