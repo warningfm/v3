80b643eb1f37782bb120245221e1728229277afc
Radio Warning FM <warningfm@gmail.com>
2023-12-31 19:40:43 +0700

    Update 27d717777f06ec.js

--- js/27d717777f06ec.js
+++ js/27d717777f06ec.js
@@ -6,23 +6,28 @@
 // =============================================== //
 
     $.kast({
-        host: 'live.sg.radiobintangtenggara.com',
+        host: 'ssg.streamingmurah.com',
         port: 9560,
-        protocol: 'http',
-        version: 1,
-        directStreamURL: 'https://ssg.streamingmurah.com:9560/rbtfm',
+        protocol: 'https',
+        version: 2,  
+        sid: 1,  
+        direct: true,  
+        betaProxies: false,
+        //directStreamURL: 'https://ssg.streamingmurah.com:9560/rbtfm',
         defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
-        statusBar: false,
+        statusBar: false, 
+        minimizeMaximize: true,
         startTemplate: 'maximized',
         language: {
             offlineText: 'Temporarily Offline',
-            playedText: '<font face="Georgia" color="blue">Wes Mari Diputer :</font>',
+            playedText: '<font face="Georgia" color="blue">Mari Diputer :</font>',
             unknownTrackText: 'Informasi & Solusi - c^o^d^e^l^i^s^t^.^c^c', 
             unknownArtistText: 'Radio Bintang Tenggara' 
         },
-        position: 'left',
-        colors: 'dynamic',
-        theme: 'dark',
+        position: 'right',
+        colors: 'dynamic',  
+        continuous: true,
+        theme: 'dynamic',
         autoPlay: true,
-        played: false
+        played: true
       })
