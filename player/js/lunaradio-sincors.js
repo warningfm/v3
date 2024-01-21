3a3238924ee54d96d88f5099b6154d846f3bd5e1
Radio Warning FM <warningfm@gmail.com>
2024-01-21 19:38:41 +0700

    Update lunaradio-sincors.js

--- player/js/lunaradio-sincors.js
+++ player/js/lunaradio-sincors.js
@@ -456,7 +456,7 @@ if (w.metadatatechnic === "corsproxy") {
                 dataType: "text",
                 method: "GET",
                 crossDomain: !0,
-                url: "https://tools.zenoradio.com/api/stations/" + zn + "/now_playing/",
+                url: "https://zenoplay.zenomedia.com/api/zenofm/nowplaying/" + zn + "/now_playing/",
                 success: function(b) {
                     try {
                         var a = JSON.parse(b);
