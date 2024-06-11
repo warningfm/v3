426be0ee83b4186b63665b9f92d46d3a48fd4782
Radio Warning FM <warningfm@gmail.com>
2024-06-12 00:29:22 +0700

    Update jembut.js

--- player/js/jembut.js
+++ player/js/jembut.js
@@ -76,7 +76,7 @@ function Page() {
             if (this.readyState === 4 && this.status === 200) {
                 var data = JSON.parse(this.responseText);
                 var artwork = data.results.artwork;
-                 var artworkXL = artwork.large;
+                 var artworkXL = artwork.medium;
 
                 document.querySelectorAll('#historicSong article .cover-historic')[n].style.backgroundImage = 'url(' + artworkXL + ')';
             }
@@ -118,7 +118,7 @@ function Page() {
             if (this.readyState === 4 && this.status === 200) {
                 var data = JSON.parse(this.responseText);
                 var artworkUrl100 = data.results;
-                var urlCoverArt = artworkUrl100.artwork.medium;
+                var urlCoverArt = artworkUrl100.artwork.xl;
 
                 coverArt.style.backgroundImage = 'url(' + urlCoverArt + ')';
                 coverArt.className = 'animated bounceInLeft';
