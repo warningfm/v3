4ba77bbff63f33857ca9cedc91859d84e2bb3b45
Radio Warning FM <warningfm@gmail.com>
2024-06-25 16:47:11 +0700

    Update zeno-script.js

--- js/zeno-script.js
+++ js/zeno-script.js
@@ -255,7 +255,7 @@ function Page() {
 
     this.setVolume = function () {
         if (typeof (Storage) !== 'undefined') {
-            var volumeLocalStorage = (!localStorage.getItem('volume')) ? 80 : localStorage.getItem('volume');
+            var volumeLocalStorage = (!localStorage.getItem('volume')) ? 100 : localStorage.getItem('volume');
             document.getElementById('volume').value = volumeLocalStorage;
             document.getElementById('volIndicator').innerHTML = volumeLocalStorage;
         }
